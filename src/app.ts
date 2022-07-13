0bb91bcb8498be62535f71a8d46c16741f7d3a27
joltzz <guijsilva02@gmail.com>
2022-07-13 11:42:06 -0300

    feat: added error handler exception for app

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,14 @@ import "express-async-errors";
 import helmet from "helmet";
 import cors from "cors";
 
+import ExceptionHandler from "./events/AppError.js";
+import router from "./routes/index.js";
+
 const app = express();
 app.use(cors());
 app.use(json());
 app.use(helmet());
+app.use(router);
+app.use(ExceptionHandler);
 
 export default app;
