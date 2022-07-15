5a25af714e7ab5f84b1011605ab027e5e493f247
joltzz <guijsilva02@gmail.com>
2022-07-15 15:58:11 -0300

    feat: credentials endpoint done.

--- src/routes/credentialRoute.ts
+++ src/routes/credentialRoute.ts
@@ -1,10 +1,17 @@
 import { Router } from "express";
 import * as credentialsController from "../controllers/credentialsController.js";
+import { tokenValidation } from "../middlewares/tokenValidationMiddleware.js";
+import { validateSchema } from "../middlewares/validateSchemaMiddleware.js";
+import { credentialSchema } from "../schemas/credentialSchema.js";
 
 const credentialsRoute = Router();
 
-credentialsRoute.post("/credential", credentialsController);
-credentialsRoute.get("/credential/?id", credentialsController);
-credentialsRoute.delete("/credential/:id", credentialsController);
+credentialsRoute.use(tokenValidation);
+
+credentialsRoute.post("/credential", validateSchema(credentialSchema),
+  credentialsController.create
+);
+// credentialsRoute.get("/credential/?id", credentialsController);
+// credentialsRoute.delete("/credential/:id", credentialsController);
 
 export default credentialsRoute;
